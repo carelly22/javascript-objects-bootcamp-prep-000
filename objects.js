5b6a98c19d76aca4b7b2667a3403ddb91d64ab4c
Carell Hamil <ayannahamil@gmail.com>
2018-11-17 23:46:19 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,8 @@
+//create an object playlist and initialize it with a key-value pair
+const playlist = {};
+playlist["Ariana Grande"] = "sweetener";
+
+//define a function to update the playlist objects
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.aasign({}. playlist, {[artistName]:songTitle})
+}
